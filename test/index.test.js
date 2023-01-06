4daaa67e478d2e8927ab525cd11a7d50d49f00ed
Scott <absentees@users.noreply.github.com>
2023-01-06 00:45:55 +0000

    write some tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,22 @@
 const axios = require("axios");
-const lib = require('../index')
+const dotenv = require('dotenv');
+const lib = require('../index');
 
+beforeAll(() => {
+	dotenv.config();	
+});
+
+test('dotenv is correctly reading environment variables', () => {
+	dotenv.config();
+  
+	expect(process.env.AUTH_TOKEN).toBeDefined();
+	expect(process.env.AUTH_TOKEN).toBe('123');
+  });
 
 test('get a specific channel', async () => {
 	const baseURL = "http://api.are.na/v2/channels/arena-influences";
 	const response = await lib.func();
 	expect(response.data.title).toBe('Arena Influences');
-	expect(response.data.id).toBe(275)
+	expect(response.data.id).toBe(275);
 })
 
