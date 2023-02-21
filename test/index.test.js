e0d04c1c738b1b797cbf0145ab6fc3131bc24bbf
Scott <absentees@users.noreply.github.com>
2023-02-21 10:55:04 +0000

    testing co-pilot

--- test/index.test.js
+++ test/index.test.js
@@ -6,13 +6,6 @@ beforeAll(() => {
 	dotenv.config();	
 });
 
-test('dotenv is correctly reading environment variables', () => {
-	dotenv.config();
-  
-	expect(process.env.AUTH_TOKEN).toBeDefined();
-	expect(process.env.AUTH_TOKEN).toBe('123');
-  });
-
 test('get a specific channel', async () => {
 	const channelURL = "http://api.are.na/v2/channels/arena-influences";
 	const response = await lib.getChannelByURL(channelURL);
@@ -20,7 +13,31 @@ test('get a specific channel', async () => {
 	expect(response.data.id).toBe(275);
 });
 
+// test to get a public channel by id
+test('get a public channel by id', async () => {
+	const channelID = 275;
+	const response = await lib.getChannelByID(channelID);
+	expect(response.data.title).toBe('Arena Influences');
+	expect(response.data.id).toBe(275);
+});
+
 test('get a private channel called toolz', async () => {
-	const response = await lib.getChannelByName('toolz');
-	expect(response.slug).toBe("toolz-bvvqxr2e3ok");
+	const channelName = "toolz";
+	const response = await lib.getChannelByName(channelName);
+	expect(response.title).toBe('toolz');
+	expect(response.slug).toBe('toolz-bvvqxr2e3ok');
+});
+
+// test('create a channel called testing123', async () => {
+// 	const response = await lib.createChannel('testing123');
+// 	expect(response.status).toBe(200);
+// 	expect(response.data.title).toBe('testing123');
+// });
+
+afterAll(async () => {
+	// delete the channel we created
+	const response = await lib.getChannelByName('testing123');
+	const channelID = response.id;
+	const deleteResponse = await lib.deleteChannel(channelID);
+	expect(deleteResponse.status).toBe(200);
 });
